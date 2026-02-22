86fabee645c6c3e2142a55905908d90ff524710b
Baidehi Samantray <baidehi.shreya@gmail.com>
2026-02-22 20:49:58 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'custom': ['Saira Condensed', 'sans-serif'],
+        'dashboard': ['Sarabun', 'sans-serif'],
+        'sansationreg': ['sansationregular'],
+        'sansationlight': ['sansationlight'],
+      },
+    },
+  },
+  plugins: [],
+}
